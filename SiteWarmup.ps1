b573708e3913e8d56dfc91e45c1907d2759a8b4e
avisra <woodard@WOODARD.telerik.com>
2016-02-22 10:25:58 -0500

    Updated logging and parameters

--- SiteWarmup.ps1
+++ SiteWarmup.ps1
@@ -1,15 +1,21 @@
+# sitemapFile - location of the XML Sitemap
+# statusFile - location of the status file. Progress is written here. Place somewhere in website root so you can open using a web browser to see current status. If empty, placed in same folder as this script
+# logFile - location of the log file. All URLs are logged here with their HTTP status code and response time. Recommend placing in App_Data. If empty, placed in same folder as this script
+
+param([String]$sitemapFile, [String]$statusFile, [String]$logFile)
+
+# Multi-threading library
 $MyDir = Split-Path $MyInvocation.MyCommand.Definition
 . "$MyDir\Invoke-Parallel.ps1"
 
-cls
-
 $startTime = Get-Date
-$sitemapFile = 'C:\path\to\sitemap.xml'
-$logFile = '.\log.csv'
-$statusFile = '.\status.txt'
-$pages = @()
 
-Clear-Content $statusFile
+if([string]::IsNullOrEmpty($logFile)) { $logFile = "$MyDir\log.csv" }
+if([string]::IsNullOrEmpty($statusFile)) { $statusFile = "$MyDir\status.txt" }
+
+$null | out-file $statusFile
+    
+$pages = @()
 
 # Collect page urls for processing
 ""
@@ -51,3 +57,5 @@ $pages | Invoke-Parallel -LogFile $logFile -ProgressFile $statusFile {
     $response.Close()
 
 }
+
+Write-Host "Warmup completed"  -ForegroundColor "magenta"
