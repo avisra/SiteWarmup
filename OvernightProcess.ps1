4937baf9aae140a27bfd5537ede192ad7fa331bc
avisra <woodard@WOODARD.telerik.com>
2016-03-17 08:24:02 -0400

    Added forced application recycle

--- OvernightProcess.ps1
+++ OvernightProcess.ps1
@@ -1,7 +1,13 @@
 $MyDir = Split-Path $MyInvocation.MyCommand.Definition
 
-$precompilerFile = "$MyDir\Telerik.Sitefinity.Compiler.1.0.0.10\Telerik.Sitefinity.Compiler.exe"
-& $precompilerFile /url="http://localhost:5960/" /appdir="C:\Path\to\application\root" /authKey="L2737ln9EieQG7T0uaWdGT5J00jRNdgl" /strategy="Frontend"
+$precompilerFile = "C:\Users\woodard\Desktop\Performance\SiteWarmup\Telerik.Sitefinity.Compiler.1.0.0.10\Telerik.Sitefinity.Compiler.exe"
+& $precompilerFile /url="http://localhost:5080" /appdir="C:\Projects\dag-sitefinity-website\SitefinityWebApp" /authKey="L2737ln9EieQG7T0uaWdGT5J00jRNdgl" /strategy="Frontend"
+
+# Please note, if you have Overlapped Recycles turned on in IIS, the application will
+# not recycle immediately (the WaitForSitefinityToStart script will think it has started
+# immediately, incorrectly). Make sure you diable overlapped recycles or manually recycle
+# the application pool here
+# Restart-WebAppPool -name ".NET v4.5"
 
 . "$MyDir\WaitForSitefinityToStart.ps1" -url "http://localhost:5960"
 . "$MyDir\SiteWarmup.ps1" -sitemapFile "C:\path\to\your\sitemap.xml"
